d524486e926b45f7c085e8103b7d5cd040583264
Obscuron <Abscuron@gmail.com>
2013-06-17 02:28:20 -0400

    Added classes for constants

---
+++ common/obscuron/mkin/lib/BlockInfo.java
@@ -0,0 +1,21 @@
+package obscuron.mkin.lib;
+
+public class BlockInfo {
+    
+    /* Current Block Ids */
+    public static int PROGRAMMER_ID = 2400;
+    public static int INTERFACE_ID = 2401;
+    
+    /* Default Block Ids */
+    public static final int DEFAULT_PROGRAMMER_ID = 2400;
+    public static final int DEFAULT_INTERFACE_ID = 2401;
+    
+    /* Unlocalized Names */
+    public static final String PROGRAMMER_NAME = "kineticProgrammer";
+    public static final String INTERFACE_NAME = "kineticInterface";
+    
+    /* Localized Names */
+    public static final String PROGRAMMER_FULLNAME = "Kinetic Programmer";
+    public static final String INTERFACE_FULLNAME = "Kinetic Interface";
+
+}
---
+++ common/obscuron/mkin/lib/ItemInfo.java
@@ -0,0 +1,5 @@
+package obscuron.mkin.lib;
+
+public class ItemInfo {
+
+}
