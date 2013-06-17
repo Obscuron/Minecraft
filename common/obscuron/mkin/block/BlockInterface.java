4b1fbdce87ed1814f5c6cd3574f8c24e65494fcc
Obscuron <Abscuron@gmail.com>
2013-06-17 15:08:55 -0400

    Added temporary textures for 2 blocks

--- common/obscuron/mkin/block/BlockInterface.java
+++ common/obscuron/mkin/block/BlockInterface.java
@@ -1,21 +1,43 @@
 package obscuron.mkin.block;
 
-import obscuron.mkin.ModularKinetics;
-import obscuron.mkin.lib.BlockInfo;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
+import obscuron.mkin.ModularKinetics;
+import obscuron.mkin.lib.BlockInfo;
+import obscuron.mkin.lib.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockInterface extends BlockContainer {
 
+    @SideOnly(Side.CLIENT)
+    private Icon[] textures;
+    
     public BlockInterface(int blockId) {
         super(blockId, Material.iron);
         this.setHardness(2.0F);
         this.setUnlocalizedName(BlockInfo.INTERFACE_NAME);
         this.setCreativeTab(ModularKinetics.tabKinetics);
     }
-
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister iconRegister) {
+        textures = new Icon[6];
+        for (int i=0; i<6; i++) {
+            textures[i] = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName2() + i);
+        }
+    }
+    
+    @Override
+    public Icon getIcon(int side, int metadata) {
+        return textures[side];
+    }
+    
     @Override
     public TileEntity createNewTileEntity(World world) {
         return null;
--- common/obscuron/mkin/block/BlockProgrammer.java
+++ common/obscuron/mkin/block/BlockProgrammer.java
@@ -1,14 +1,22 @@
 package obscuron.mkin.block;
 
-import obscuron.mkin.ModularKinetics;
-import obscuron.mkin.lib.BlockInfo;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
+import obscuron.mkin.ModularKinetics;
+import obscuron.mkin.lib.BlockInfo;
+import obscuron.mkin.lib.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockProgrammer extends BlockContainer {
 
+    @SideOnly(Side.CLIENT)
+    private Icon[] textures;
+    
     public BlockProgrammer(int blockId) {
         super(blockId, Material.iron);
         this.setHardness(2.0F);
@@ -16,6 +24,21 @@ public class BlockProgrammer extends BlockContainer {
         this.setCreativeTab(ModularKinetics.tabKinetics);
     }
 
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister iconRegister) {
+        textures = new Icon[3];
+        for (int i=0; i<3; i++) {
+            String suffix = i == 0 ? "Bot" : i == 1 ? "Top" : "Side";
+            textures[i] = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName2() + suffix);
+        }
+    }
+    
+    @Override
+    public Icon getIcon(int side, int metadata) {
+        return textures[side > 1 ? 2 : side];
+    }
+    
     @Override
     public TileEntity createNewTileEntity(World world) {
         return null;
